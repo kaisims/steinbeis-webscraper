bbf21fa34b3f56116aae34185fd67d052d35dcfa
Kai R <kaisims@users.noreply.github.com>
2022-02-15 16:39:37 +0100

    asd

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 printenv > /etc/environment
 
-echo "start cronjob"
-cron && tail -f /var/log/eis-scraper.log
 echo "start node server"
 node index.js
+echo "start cronjob"
+cron && tail -f /var/log/eis-scraper.log
